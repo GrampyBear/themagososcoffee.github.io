d3459c5d8a9e11e9a4227f3121c161b3bff4b506
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-08-16 15:59:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const audioPlayer = document.getElementById('audioPlayer');
 const audioLinks = [
   // Coloca aquí los enlaces de YouTube
+  // Ejemplo: 'https://www.youtube.com/watch?v=VIDEO_ID'
   'https://www.youtube.com/watch?v=DfyiWf88iHw'
 ];
 
@@ -11,10 +12,11 @@ function playAudio(index) {
   const videoId = link.match(/[?&]v=([^&#]+)/)[1];
   const embedUrl = `https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&autohide=1&mute=1`;
 
-  audioPlayer.src = embedUrl;
-  audioPlayer.allow = 'autoplay';
-  audioPlayer.frameborder = '0';
-  audioPlayer.allowfullscreen = true;
+  const iframe = document.createElement('iframe');
+  iframe.src = embedUrl;
+  iframe.style.display = 'none';
+  audioPlayer.innerHTML = '';
+  audioPlayer.appendChild(iframe);
 }
 
 playAudio(currentTrackIndex);
