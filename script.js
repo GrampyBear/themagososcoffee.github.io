b316076a6622a48dedbf75036a916c3d454fce57
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-08-16 15:03:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,20 @@
 const audioPlayer = document.getElementById('audioPlayer');
 const audioLinks = [
-  // Coloca aquí los enlaces de YouTube, Spotify y SoundCloud
-  // Ejemplo: 'https://www.youtube.com/watch?v=VIDEO_ID'
-  'https://youtu.be/DfyiWf88iHw'
+  // Coloca aquí los enlaces de YouTube
+  'https://www.youtube.com/watch?v=DfyiWf88iHw'
 ];
 
 let currentTrackIndex = 0;
 
 function playAudio(index) {
   const link = audioLinks[index];
-  if (link.includes('youtube.com') || link.includes('youtu.be')) {
-    // Si es un enlace de YouTube
-    const videoId = link.match(/[?&]v=([^&#]+)/)[1];
-    audioPlayer.src = `https://www.youtube.com/embed/${videoId}`;
-  } else {
-    // Para otros enlaces (Spotify, SoundCloud, etc.)
-    audioPlayer.src = link;
-  }
-  audioPlayer.play();
-}
+  const videoId = link.match(/[?&]v=([^&#]+)/)[1];
+  const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0`;
 
-function changeTrack(direction) {
-  currentTrackIndex += direction;
-  if (currentTrackIndex < 0) {
-    currentTrackIndex = audioLinks.length - 1;
-  } else if (currentTrackIndex >= audioLinks.length) {
-    currentTrackIndex = 0;
-  }
-  playAudio(currentTrackIndex);
+  audioPlayer.src = embedUrl;
+  audioPlayer.allow = 'autoplay';
+  audioPlayer.frameborder = '0';
+  audioPlayer.allowfullscreen = true;
 }
 
 playAudio(currentTrackIndex);
-
-audioPlayer.addEventListener('ended', () => {
-  changeTrack(1); // Reproduce la siguiente canción al finalizar la actual
-});
