299002b0bad8d2a72a993902afb0b553027ffa06
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-08-16 17:07:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const commentSection = document.getElementById("comentarios");
 
-  function agregarComentario(nombre, comentario) {
-    const comentarioElemento = document.createElement("div");
-    comentarioElemento.classList.add("comentario");
-    comentarioElemento.innerHTML = `<strong>${nombre}:</strong> ${comentario}`;
-    commentSection.appendChild(comentarioElemento);
-  }
-
-  const commentForm = document.getElementById("comment-form");
-  commentForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const nombre = document.getElementById("nombre").value;
-    const comentario = document.getElementById("comentario").value;
-    agregarComentario(nombre, comentario);
-    commentForm.reset();
-  });
-});
