c9d27f203ec0f3c5402c4a4bc9939745a040defa
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-09 22:19:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
-// Function to update styles dynamically based on time of day
-function updateStylesBasedOnTime() {
+// Function to update time dynamically in the navbar
+function updateTime() {
+    const timeOfDayElement = document.getElementById('timeOfDay');
+    const exactTimeElement = document.getElementById('exactTime');
     const currentTime = new Date();
-    const hours = currentTime.getHours();
-    const navbarTitle = document.querySelector('.navbar-title');
-    const pageTitle = document.querySelector('.page-title');
-    const timeText = document.querySelectorAll('.time-text');
-    const footerText = document.querySelector('footer');
 
-    let textColor;
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+
+    // Format minutes with leading zero if needed
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    let timeOfDay;
 
     if (hours >= 6 && hours < 12) {
-        // Morning
-        textColor = '#000'; // Black text for better contrast
+        timeOfDay = 'Morning';
+        setColors('#FFD700', '#ffffff'); // Set colors for morning
     } else if (hours >= 12 && hours < 17) {
-        // Afternoon
-        textColor = '#000'; // Black text for better contrast
+        timeOfDay = 'Afternoon';
+        setColors('#87CEEB', '#ffffff'); // Set colors for afternoon
     } else if (hours >= 17 && hours < 20) {
-        // Evening
-        textColor = '#fff'; // White text for better contrast
+        timeOfDay = 'Evening';
+        setColors('#FF6347', '#ffffff'); // Set colors for evening
     } else if (hours >= 20 && hours < 24) {
-        // Night
-        textColor = '#fff'; // White text for better contrast
+        timeOfDay = 'Night';
+        setColors('#483D8B', '#ffffff'); // Set colors for night
     } else {
-        // Midnight
-        textColor = '#fff'; // White text for better contrast
+        timeOfDay = 'Midnight';
+        setColors('#191970', '#ffffff'); // Set colors for midnight
     }
 
-    // Apply the text color dynamically
-    navbarTitle.style.color = textColor;
-    pageTitle.style.color = textColor;
-
-    // Loop through timeText elements and update color
-    timeText.forEach((text) => {
-        text.style.color = textColor;
-    });
-
-    // Apply color to footer text
-    footerText.style.color = textColor;
+    timeOfDayElement.textContent = `Time of Day: ${timeOfDay}`;
+    exactTimeElement.textContent = `Exact Time: ${hours}:${minutes}`;
 }
 
-// Initial call to update styles based on time
-updateStylesBasedOnTime();
+// Function to set text and background colors
+function setColors(textColor, backgroundColor) {
+    document.body.style.background = `linear-gradient(to bottom, ${backgroundColor})`;
+    document.body.style.color = textColor;
+}
 
-// Update styles every second for real-time adjustments
-setInterval(updateStylesBasedOnTime, 1000);
-                     
+// Initial call to display time and set initial colors
+updateTime();
+
+// Update time every second for real-time clock
+setInterval(updateTime, 1000);
+
+// Function to handle color transition on page load
+function handlePageLoadTransition() {
+    document.body.style.transition = 'background 1s ease-in-out';
+    // Triggering the transition by updating the background after a slight delay
+    setTimeout(() => {
+        updateTime();
+    }, 50);
+}
+
+// Initial call to handle color transition on page load
+handlePageLoadTransition();
