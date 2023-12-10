25ce7729492681d6ebee911f3fbabc4ba2acec39
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-09 22:17:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,47 @@
-// Function to update time dynamically in the navbar
-function updateTime() {
-    const timeOfDayElement = document.getElementById('timeOfDay');
-    const exactTimeElement = document.getElementById('exactTime');
-    const body = document.body;
+// Function to update styles dynamically based on time of day
+function updateStylesBasedOnTime() {
     const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const navbarTitle = document.querySelector('.navbar-title');
+    const pageTitle = document.querySelector('.page-title');
+    const timeText = document.querySelectorAll('.time-text');
+    const footerText = document.querySelector('footer');
 
-    let hours = currentTime.getHours();
-    let minutes = currentTime.getMinutes();
-
-    // Format minutes with leading zero if needed
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-
-    let timeOfDay;
+    let textColor;
 
     if (hours >= 6 && hours < 12) {
-        timeOfDay = 'Morning';
-        body.style.background = 'linear-gradient(to bottom, #FFD700, #ffffff)';
-        setTextColor(body, '#333'); // Default text color for morning
+        // Morning
+        textColor = '#000'; // Black text for better contrast
     } else if (hours >= 12 && hours < 17) {
-        timeOfDay = 'Afternoon';
-        body.style.background = 'linear-gradient(to bottom, #87CEEB, #ffffff)';
-        setTextColor(body, '#333'); // Default text color for afternoon
+        // Afternoon
+        textColor = '#000'; // Black text for better contrast
     } else if (hours >= 17 && hours < 20) {
-        timeOfDay = 'Evening';
-        body.style.background = 'linear-gradient(to bottom, #FF6347, #ffffff)';
-        setTextColor(body, '#fff'); // White text color for evening
+        // Evening
+        textColor = '#fff'; // White text for better contrast
     } else if (hours >= 20 && hours < 24) {
-        timeOfDay = 'Night';
-        body.style.background = 'linear-gradient(to bottom, #483D8B, #ffffff)';
-        setTextColor(body, '#fff'); // White text color for night
+        // Night
+        textColor = '#fff'; // White text for better contrast
     } else {
-        timeOfDay = 'Midnight';
-        body.style.background = 'linear-gradient(to bottom, #191970, #ffffff)';
-        setTextColor(body, '#fff'); // White text color for midnight
+        // Midnight
+        textColor = '#fff'; // White text for better contrast
     }
 
-    timeOfDayElement.textContent = `Time of Day: ${timeOfDay}`;
-    exactTimeElement.textContent = `Exact Time: ${hours}:${minutes}`;
+    // Apply the text color dynamically
+    navbarTitle.style.color = textColor;
+    pageTitle.style.color = textColor;
+
+    // Loop through timeText elements and update color
+    timeText.forEach((text) => {
+        text.style.color = textColor;
+    });
+
+    // Apply color to footer text
+    footerText.style.color = textColor;
 }
 
-// Function to set text color based on background color
-function setTextColor(element, bgColor) {
-    const brightness = getBrightness(bgColor);
+// Initial call to update styles based on time
+updateStylesBasedOnTime();
 
-    // Adjusting text color based on brightness
-    element.style.color = brightness > 128 ? '#333' : '#fff';
-}
-
-// Function to get brightness of a color
-function getBrightness(color) {
-    const rgb = color.match(/\d+/g);
-    const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
-    return brightness;
-}
-
-// Initial call to display time
-updateTime();
-
-// Update time every second for real-time clock
-setInterval(updateTime, 1000);
-
-// Function to handle color transition on page load
-function handlePageLoadTransition() {
-    document.body.style.transition = 'background 1s ease-in-out';
-    // Triggering the transition by updating the background after a slight delay
-    setTimeout(() => {
-        updateTime();
-    }, 50);
-}
-
-// Initial call to handle color transition on page load
-handlePageLoadTransition();
-               
+// Update styles every second for real-time adjustments
+setInterval(updateStylesBasedOnTime, 1000);
+                     
