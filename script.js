cfb6fb527aa16a71a8694281228932f8e81ee8c7
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-10 00:59:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Function to update time dynamically in the navbar
+// Function to update time dynamically in the navbar
 function updateTime() {
     const timeOfDayElement = document.getElementById('timeOfDay');
     const currentTimeElement = document.getElementById('currentTime');
@@ -14,23 +15,53 @@ function updateTime() {
 
     if (hours >= 6 && hours < 12) {
         timeOfDay = 'Morning';
-        updateColors('sunrise'); // Update text and background colors
+        updateColors('sunrise');
+        removeStars(); // Remove stars for other times
     } else if (hours >= 12 && hours < 17) {
         timeOfDay = 'Afternoon';
         updateColors('noon');
+        removeStars();
     } else if (hours >= 17 && hours < 20) {
         timeOfDay = 'Evening';
         updateColors('sunset');
+        removeStars();
     } else if (hours >= 20 && hours < 24) {
         timeOfDay = 'Night';
         updateColors('dusk');
+        if (hours === 20) {
+            generateStars(); // Generate stars at the beginning of the night
+        }
     } else {
         timeOfDay = 'Midnight';
         updateColors('midnight');
+        generateStars(); // Generate stars at midnight
     }
 
     timeOfDayElement.textContent = `Time of Day: ${timeOfDay}`;
     currentTimeElement.textContent = `Current Time: ${hours}:${minutes}`;
+}
+
+// Function to generate twinkling stars
+function generateStars() {
+    const starsContainer = document.createElement('div');
+    starsContainer.className = 'stars-container';
+
+    for (let i = 0; i < 50; i++) {
+        const star = document.createElement('div');
+        star.className = 'star';
+        star.style.animationDelay = `${Math.random() * 5}s`;
+        starsContainer.appendChild(star);
+    }
+
+    document.body.appendChild(starsContainer);
+}
+
+// Function to remove stars
+function removeStars() {
+    const starsContainer = document.querySelector('.stars-container');
+    if (starsContainer) {
+        starsContainer.remove();
+    }
 }
 
 // Function to update text and background colors
