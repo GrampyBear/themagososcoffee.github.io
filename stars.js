153a510b93de80603291efe488dc32e8987bb81a
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-12 22:38:11 -0300

    Update stars.js

--- stars.js
+++ stars.js
@@ -20,9 +20,6 @@ function updateStarsVisibility(timeOfDay) {
         starsContainer.style.display = 'none';
     }
 }
-// Store initial positions of stars
-const initialPositions = new Set();
-
 // Generate stars dynamically
 function generateStars() {
     // Clear existing stars
@@ -45,41 +42,18 @@ function generateStarsInLayer(count, className, disappearanceDuration) {
     for (let i = 0; i < count; i++) {
         const star = document.createElement('div');
         star.className = `star ${className}`;
-        const position = getRandomPosition();
-        star.style.top = position.top;
-        star.style.left = position.left;
+        star.style.top = Math.random() * 100 + 'vh';
+        star.style.left = Math.random() * 100 + 'vw';
         starsContainer.appendChild(star);
-
-        // Apply a timeout for the fade-out effect after the initial appearance
-        setTimeout(() => {
-            star.style.opacity = 0;
-        }, 1000); // Adjust the timeout duration (in milliseconds) as needed
     }
-}
 
-// Generate a random position that hasn't been used before
-function getRandomPosition() {
-    let top, left;
-    do {
-        top = Math.random() * 100 + 'vh';
-        left = Math.random() * 100 + 'vw';
-    } while (isPositionUsed(top, left));
-
-    // Store the initial position
-    initialPositions.push({ top, left });
-
-    return { top, left };
-}
-
-// Check if a position is already used
-function isPositionUsed(top, left) {
-    return initialPositions.some(position => {
-        const tolerance = 0.1; // Adjust tolerance as needed
-        return (
-            Math.abs(position.top - parseFloat(top)) < tolerance &&
-            Math.abs(position.left - parseFloat(left)) < tolerance
-        );
-    });
+    // Apply a timeout for the fade-out effect after the initial appearance
+    setTimeout(() => {
+        const stars = document.querySelectorAll(`.star.${className}`);
+        stars.forEach((star) => {
+            star.style.opacity = 0;
+        });
+    }, 1000); // Adjust the timeout duration (in milliseconds) as needed
 }
 
 // Initial stars generation
