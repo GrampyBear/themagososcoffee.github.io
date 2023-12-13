85b2b40ec43fd8c83db11597a36bbf73810b75a7
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-13 11:03:23 -0300

    Update stars.js

--- stars.js
+++ stars.js
@@ -1,45 +1,82 @@
-// Create a star
-function createStar(size) {
+// stars.js
+
+// Function to generate a random number within a given range
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+// Function to create a star element
+function createStar() {
     const star = document.createElement('div');
-    star.className = `star star-${size}`;
+    star.className = 'star';
+
+    // Set random position and size
+    star.style.left = `${getRandomNumber(0, 100)}vw`;
+    star.style.top = `${getRandomNumber(0, 100)}vh`;
+    star.style.width = `${getRandomNumber(1, 3)}px`;
+    star.style.height = `${getRandomNumber(1, 3)}px`;
+
     return star;
 }
 
-// Generate random position
-function getRandomPosition() {
-    const position = {
-        top: `${Math.random() * 100}%`,
-        left: `${Math.random() * 100}%`,
-    };
-    return position;
-}
-
-// Add stars to a container with a specific depth class
-function addStars(container, count, size, depthClass) {
-    for (let i = 0; i < count; i++) {
-        const star = createStar(size);
-        const position = getRandomPosition();
-        star.style.top = position.top;
-        star.style.left = position.left;
-        container.appendChild(star);
-    }
-
-    // Add depth class to the container
-    container.classList.add(depthClass);
-}
-
-// Generate stars for each layer during night and midnight
-function generateStars() {
-    const timeOfDay = getTimeOfDay();
-
-    if (timeOfDay === 'night' || timeOfDay === 'midnight') {
-        addStars(document.getElementById('stars-container1'), 5, 'large', 'depth1');
-        addStars(document.getElementById('stars-container2'), 15, 'medium', 'depth2');
-        addStars(document.getElementById('stars-container3'), 30, 'small', 'depth3');
+// Function to append stars to the body
+function appendStars(numStars) {
+    const body = document.body;
+    for (let i = 0; i < numStars; i++) {
+        const star = createStar();
+        body.appendChild(star);
     }
 }
 
-// Call the function to generate stars after the page has loaded
-document.addEventListener("DOMContentLoaded", function () {
-    generateStars();
-});
+// Function to make stars twinkle
+function twinkleStars() {
+    const stars = document.querySelectorAll('.star');
+    stars.forEach((star) => {
+        // Randomly toggle visibility
+        if (Math.random() > 0.5) {
+            star.style.opacity = '0';
+        } else {
+            star.style.opacity = '1';
+        }
+    });
+}
+
+// Check if it's night or midnight
+function isNightOrMidnight(timeOfDay) {
+    return timeOfDay === 'night' || timeOfDay === 'midnight';
+}
+
+// Check time of day and initialize stars if it's night or midnight
+function initializeStars(timeOfDay) {
+    if (isNightOrMidnight(timeOfDay)) {
+        const numStars = 100; // Adjust the number of stars as needed
+        appendStars(numStars);
+        setInterval(twinkleStars, 1000); // Twinkle stars every second
+    }
+}
+
+// Function to fetch time of day from the main script
+function getTimeOfDay() {
+    return document.getElementById('timeOfDay').textContent.split(': ')[1].toLowerCase();
+}
+
+// Initialize stars based on the time of day
+initializeStars(getTimeOfDay());
+
+// Periodically check the time of day and adjust stars
+setInterval(() => {
+    const newTimeOfDay = getTimeOfDay();
+    const stars = document.querySelectorAll('.star');
+
+    if (isNightOrMidnight(newTimeOfDay)) {
+        // Stars should be active during the night or midnight
+        stars.forEach((star) => {
+            star.style.display = 'block';
+        });
+    } else {
+        // Stars should be hidden during other times of the day
+        stars.forEach((star) => {
+            star.style.display = 'none';
+        });
+    }
+}, 60000); // Check every minute (adjust as needed)
