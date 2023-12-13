06947846553e4c390124766b375f50313b914365
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-13 20:39:24 -0300

    Update block1.js

--- block1.js
+++ block1.js
@@ -1,1 +1,15 @@
+// Add this function to show/hide sections based on the section ID
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach((section) => {
+        section.style.display = 'none'; // Hide all sections
+    });
+
+    const targetSection = document.getElementById(sectionId);
+    if (targetSection) {
+        targetSection.style.display = 'block'; // Show the target section
+    }
+}
+
+// Example usage: showSection('section1');
 
