9510d3114dd279ae218cebfaf8d372ad11d386ee
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-14 17:49:28 -0300

    Update block1.js

--- block1.js
+++ block1.js
@@ -1,16 +1,9 @@
 // main-script.js
 
-// Function to hide the dropdown content
-function hideDropdown() {
-    const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.style.display = 'none';
-}
-
-// Function to toggle drop-down visibility
-function toggleDropdown() {
-    const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
-}
+// Initialize home as active initially
+document.addEventListener('DOMContentLoaded', () => {
+    showSection('home');
+});
 
 // Function to toggle section visibility with fade effect
 function showSection(sectionClass) {
@@ -51,29 +44,64 @@ function showSection(sectionClass) {
             }, 0); // No delay for fade-in effect
         }
     }
-
-    // Hide the dropdown content when navigating to a new section
-    hideDropdown();
 }
 
-// Initialize home as active initially
-document.addEventListener('DOMContentLoaded', () => {
-    showSection('home');
-});
-
-// Event listener for the dropdown button
-document.querySelector('.dropdown-btn').addEventListener('click', toggleDropdown);
+// Function to toggle drop-down visibility
+function toggleDropdown() {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
 
 // Event listeners for navigation buttons
-document.getElementById('nav-home').addEventListener('click', () => showSection('home'));
-document.getElementById('nav-my-blog').addEventListener('click', () => showSection('my-blog'));
-document.getElementById('nav-furry-fat-gallery').addEventListener('click', () => showSection('furry-fat-gallery'));
-document.getElementById('nav-kitchen').addEventListener('click', () => showSection('kitchen'));
-document.getElementById('nav-events').addEventListener('click', () => showSection('events'));
-document.getElementById('nav-wiki').addEventListener('click', () => showSection('wiki'));
-document.getElementById('nav-curiosities-extras').addEventListener('click', () => showSection('curiosities-extras'));
-document.getElementById('nav-about-me').addEventListener('click', () => showSection('about-me'));
-document.getElementById('nav-special-functions').addEventListener('click', () => showSection('special-functions'));
-document.getElementById('nav-disclaimer').addEventListener('click', () => showSection('disclaimer'));
+document.getElementById('nav-home').addEventListener('click', () => {
+    showSection('home');
+    toggleDropdown(); // Close dropdown after selecting a section
+});
+
+document.getElementById('nav-my-blog').addEventListener('click', () => {
+    showSection('my-blog');
+    toggleDropdown();
+});
+
+document.getElementById('nav-furry-fat-gallery').addEventListener('click', () => {
+    showSection('furry-fat-gallery');
+    toggleDropdown();
+});
+
+document.getElementById('nav-kitchen').addEventListener('click', () => {
+    showSection('kitchen');
+    toggleDropdown();
+});
+
+document.getElementById('nav-events').addEventListener('click', () => {
+    showSection('events');
+    toggleDropdown();
+});
+
+document.getElementById('nav-wiki').addEventListener('click', () => {
+    showSection('wiki');
+    toggleDropdown();
+});
+
+document.getElementById('nav-curiosities-extras').addEventListener('click', () => {
+    showSection('curiosities-extras');
+    toggleDropdown();
+});
+
+document.getElementById('nav-about-me').addEventListener('click', () => {
+    showSection('about-me');
+    toggleDropdown();
+});
+
+document.getElementById('nav-special-functions').addEventListener('click', () => {
+    showSection('special-functions');
+    toggleDropdown();
+});
+
+document.getElementById('nav-disclaimer').addEventListener('click', () => {
+    showSection('disclaimer');
+    toggleDropdown();
+});
 
 // ... (remaining code)
+                                                        
