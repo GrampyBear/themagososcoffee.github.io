21e3b28fbebdd16e55d83b942b4982ecc2776994
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-14 18:01:34 -0300

    Update block1.js

--- block1.js
+++ block1.js
@@ -1,13 +1,12 @@
-// main-script.js
 
-// Initialize home as active initially
-document.addEventListener('DOMContentLoaded', () => {
-    showSection('home');
-});
+// ... (previous code)
 
 // Function to toggle section visibility with fade effect
 function showSection(sectionClass) {
     const sections = document.querySelectorAll('.section');
+
+    // Hide the dropdown content when navigating to a new section
+    hideDropdown();
 
     // Find the current visible section
     const currentVisibleSection = document.querySelector('.section[style="display: block;"]');
@@ -46,62 +45,30 @@ function showSection(sectionClass) {
     }
 }
 
-// Function to toggle drop-down visibility
-function toggleDropdown() {
-    const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+// ... (remaining code)
+
+// Hide the dropdown content when navigating to a new section
+    hideDropdown();
 }
 
-// Event listeners for navigation buttons
-document.getElementById('nav-home').addEventListener('click', () => {
+// Initialize home as active initially
+document.addEventListener('DOMContentLoaded', () => {
     showSection('home');
-    toggleDropdown(); // Close dropdown after selecting a section
 });
 
-document.getElementById('nav-my-blog').addEventListener('click', () => {
-    showSection('my-blog');
-    toggleDropdown();
-});
+// Event listener for the dropdown button
+document.querySelector('.dropdown-btn').addEventListener('click', toggleDropdown);
 
-document.getElementById('nav-furry-fat-gallery').addEventListener('click', () => {
-    showSection('furry-fat-gallery');
-    toggleDropdown();
-});
-
-document.getElementById('nav-kitchen').addEventListener('click', () => {
-    showSection('kitchen');
-    toggleDropdown();
-});
-
-document.getElementById('nav-events').addEventListener('click', () => {
-    showSection('events');
-    toggleDropdown();
-});
-
-document.getElementById('nav-wiki').addEventListener('click', () => {
-    showSection('wiki');
-    toggleDropdown();
-});
-
-document.getElementById('nav-curiosities-extras').addEventListener('click', () => {
-    showSection('curiosities-extras');
-    toggleDropdown();
-});
-
-document.getElementById('nav-about-me').addEventListener('click', () => {
-    showSection('about-me');
-    toggleDropdown();
-});
-
-document.getElementById('nav-special-functions').addEventListener('click', () => {
-    showSection('special-functions');
-    toggleDropdown();
-});
-
-document.getElementById('nav-disclaimer').addEventListener('click', () => {
-    showSection('disclaimer');
-    toggleDropdown();
-});
+// Event listeners for navigation buttons
+document.getElementById('nav-home').addEventListener('click', () => showSection('home'));
+document.getElementById('nav-my-blog').addEventListener('click', () => showSection('my-blog'));
+document.getElementById('nav-furry-fat-gallery').addEventListener('click', () => showSection('furry-fat-gallery'));
+document.getElementById('nav-kitchen').addEventListener('click', () => showSection('kitchen'));
+document.getElementById('nav-events').addEventListener('click', () => showSection('events'));
+document.getElementById('nav-wiki').addEventListener('click', () => showSection('wiki'));
+document.getElementById('nav-curiosities-extras').addEventListener('click', () => showSection('curiosities-extras'));
+document.getElementById('nav-about-me').addEventListener('click', () => showSection('about-me'));
+document.getElementById('nav-special-functions').addEventListener('click', () => showSection('special-functions'));
+document.getElementById('nav-disclaimer').addEventListener('click', () => showSection('disclaimer'));
 
 // ... (remaining code)
-                                                        
