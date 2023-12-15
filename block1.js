a9bb8b320e5d9bf3538ce4d800a96e2e5e284df5
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-15 11:56:34 -0300

    Update block1.js

--- block1.js
+++ block1.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Espera a que la página se cargue completamente
-
-    // Desvanecer la capa blanca
-    setTimeout(function () {
-        document.getElementById("overlay").style.opacity = 0;
-    }, 1000); // Después de 1 segundo (ajusta según sea necesario)
-});
 
