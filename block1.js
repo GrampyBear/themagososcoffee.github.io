aaad54dadb696610192230eb366923fd2d4c6b9e
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-14 22:01:30 -0300

    Update block1.js

--- block1.js
+++ block1.js
@@ -13,9 +13,31 @@ function hideDropdown() {
 // Function to toggle drop-down visibility with fade effect
 function toggleDropdown() {
     const dropdownContent = document.querySelector('.dropdown-content');
+    const dropdownBtn = document.querySelector('.dropdown-btn');
+    const navLinks = document.querySelectorAll('.nav-link');
+
     if (dropdownContent.style.display === 'block') {
-        hideDropdown();
+        // Hide the dropdown content with fade-out effect
+        dropdownContent.style.transition = 'opacity 0.5s, transform 0.5s';
+        dropdownContent.style.opacity = '0';
+        dropdownContent.style.transform = 'translateY(100%)';
+
+        // Set a timeout to hide the dropdown content after the fade-out
+        setTimeout(() => {
+            dropdownContent.style.display = 'none';
+        }, 500); // 0.5 seconds fade-out effect
+
+        // Set the dropdown button back to normal state
+        dropdownBtn.style.borderRadius = '50%';
+        dropdownBtn.style.background = 'linear-gradient(#d9534f, #ac2925)';
+        dropdownBtn.textContent = 'more~';
+
+        // Set nav links back to visible
+        navLinks.forEach(link => {
+            link.style.visibility = 'visible';
+        });
     } else {
+        // Show the dropdown content with fade-in effect
         dropdownContent.style.transition = 'opacity 0.5s, transform 0.5s';
         dropdownContent.style.display = 'block';
         dropdownContent.style.opacity = '1';
@@ -23,10 +45,20 @@ function toggleDropdown() {
 
         // Set a timeout to show the dropdown content after the fade-in
         setTimeout(() => {
-            dropdownContent.style.transform = 'translateY(100%)';
+            // Set the dropdown button to expanded state
+            dropdownBtn.style.borderRadius = '20% 20% 0 0';
+            dropdownBtn.style.background = 'linear-gradient(#ac2925, #d9534f)';
+            dropdownBtn.textContent = 'less~';
+
+            // Set nav links to invisible
+            navLinks.forEach(link => {
+                link.style.visibility = 'hidden';
+            }
+            );
         }, 0); // No delay for fade-in effect
     }
 }
+
 
 // Function to toggle section visibility with fade effect
 function showSection(sectionClass) {
