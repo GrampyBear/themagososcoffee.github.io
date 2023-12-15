ca5c91f217dd4f480b9b9a73c93e29c26f3cbfde
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-14 23:19:19 -0300

    Update block1.js

--- block1.js
+++ block1.js
@@ -1,31 +1,36 @@
-// main-script.js
+// ... (previous code)
 
-// Function to hide the dropdown content
-function hideDropdown() {
-    const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.style.display = 'none';
+// Function to toggle section visibility with fade effect
+function showSection(sectionClass) {
+    const sections = document.querySelectorAll('.section');
+    
+    // Find the current active section
+    const currentActiveSection = document.querySelector('.nav-button.active');
+    
+    // If there's a current active section
+    if (currentActiveSection) {
+        // Remove the active class from the current active section link
+        currentActiveSection.classList.remove('active');
+    }
+
+    // Activate the clicked section link
+    const clickedSectionLink = document.getElementById(`nav-${sectionClass}`);
+    clickedSectionLink.classList.add('active');
+
+    // Hide all sections
+    sections.forEach(section => {
+        section.style.transition = 'opacity 0.5s';
+        section.style.opacity = '0';
+        section.style.display = 'none';
+    });
+
+    // Show the target section after hiding others
+    const targetSection = document.querySelector(`.${sectionClass}`);
+    targetSection.style.transition = 'opacity 0.5s';
+    targetSection.style.display = 'block';
+
+    // Set a timeout to show the section after the fade-in
+    setTimeout(() => {
+        targetSection.style.opacity = '1';
+    }, 0); // No delay for fade-in effect
 }
-
-// Function to toggle drop-down visibility
-function toggleDropdown() {
-    const dropdownContent = document.querySelector('.dropdown-content');
-    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
-}
-
-
-// Event listener for the dropdown button
-document.querySelector('.dropdown-btn').addEventListener('click', toggleDropdown);
-
-// Event listeners for navigation buttons
-document.getElementById('nav-home').addEventListener('click', () => showSection('home'));
-document.getElementById('nav-my-blog').addEventListener('click', () => showSection('my-blog'));
-document.getElementById('nav-furry-fat-gallery').addEventListener('click', () => showSection('furry-fat-gallery'));
-document.getElementById('nav-kitchen').addEventListener('click', () => showSection('kitchen'));
-document.getElementById('nav-events').addEventListener('click', () => showSection('events'));
-document.getElementById('nav-wiki').addEventListener('click', () => showSection('wiki'));
-document.getElementById('nav-curiosities-extras').addEventListener('click', () => showSection('curiosities-extras'));
-document.getElementById('nav-about-me').addEventListener('click', () => showSection('about-me'));
-document.getElementById('nav-special-functions').addEventListener('click', () => showSection('special-functions'));
-document.getElementById('nav-disclaimer').addEventListener('click', () => showSection('disclaimer'));
-
-// ... (remaining code)
