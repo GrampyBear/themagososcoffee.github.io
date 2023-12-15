3d5e4c9d2d5a504b8ee125115d218c540800f438
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-14 23:23:18 -0300

    Update block1.js

--- block1.js
+++ block1.js
@@ -1,12 +1,14 @@
+// main-script.js
+
 // ... (previous code)
 
 // Function to toggle section visibility with fade effect
 function showSection(sectionClass) {
     const sections = document.querySelectorAll('.section');
-    
+
     // Find the current active section
     const currentActiveSection = document.querySelector('.nav-button.active');
-    
+
     // If there's a current active section
     if (currentActiveSection) {
         // Remove the active class from the current active section link
@@ -19,14 +21,14 @@ function showSection(sectionClass) {
 
     // Hide all sections
     sections.forEach(section => {
-        section.style.transition = 'opacity 0.5s';
+        section.style.transition = 'opacity 1s'; // Updated to 1 second
         section.style.opacity = '0';
         section.style.display = 'none';
     });
 
     // Show the target section after hiding others
     const targetSection = document.querySelector(`.${sectionClass}`);
-    targetSection.style.transition = 'opacity 0.5s';
+    targetSection.style.transition = 'opacity 1s'; // Updated to 1 second
     targetSection.style.display = 'block';
 
     // Set a timeout to show the section after the fade-in
@@ -34,3 +36,8 @@ function showSection(sectionClass) {
         targetSection.style.opacity = '1';
     }, 0); // No delay for fade-in effect
 }
+
+// Activate the "Home" section by default
+showSection('home');
+
+// ... (remaining code)
