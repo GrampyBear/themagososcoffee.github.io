d3dba32bbf02e222711b6b2620d7d1166a538c81
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-22 13:17:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showMessage() {
-  alert("¡Bienvenido a Morgan's Fat World!");
+  alert("Disclaimer!\n\nHey hello! This page contains information and content that may not be suitable for all audiences, rated +18. As a creator, I am not responsible for any discomfort you may feel. These are my personal preferences, and they harm no one. I hope you enjoy your stay!");
 }
 
 document.querySelector(".header .extra").addEventListener("click", showMessage);
