eab6ddb40c9de8418a0955cab0a653f75eb8361e
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-22 17:01:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the clock element
-    var clockElement = document.getElementById('clock');
+// Agregaremos el script para mostrar la hora y el momento del día
 
-    // Get the clock container element
-    var clockContainerElement = document.getElementById('clock-container');
+function updateClock() {
+    const clockContainer = document.querySelector('.clock-container');
+    const timeOfDayElement = document.getElementById('time-of-day');
+    const currentTimeElement = document.getElementById('current-time');
 
-    // Function to get the current time and day moment
-    function getTimeAndDayMoment() {
-        var currentDate = new Date();
-        var hours = currentDate.getHours();
-        var minutes = currentDate.getMinutes();
-        var seconds = currentDate.getSeconds();
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
 
-        // Determine the day moment
-        var dayMoment = getDayMoment(hours);
-
-        // Format the current time
-        var formattedTime = formatTime(hours, minutes, seconds);
-
-        // Display the result in the clock element
-        clockElement.textContent = formattedTime + ' ' + dayMoment;
-
-        // Display the result in the clock container
-        displayClockInfo(dayMoment, formattedTime);
-
-        // Update every second
-        setTimeout(getTimeAndDayMoment, 1000);
+    // Determinar el momento del día
+    let timeOfDay;
+    if (hours >= 5 && hours < 7) {
+        timeOfDay = 'Dawn';
+    } else if (hours >= 7 && hours < 12) {
+        timeOfDay = 'Morning';
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = 'Midday';
+    } else if (hours >= 17 && hours < 20) {
+        timeOfDay = 'Afternoon';
+    } else if (hours >= 20 && hours < 22) {
+        timeOfDay = 'Dusk';
+    } else if ((hours >= 22 && hours <= 23) || (hours >= 0 && hours < 5)) {
+        timeOfDay = 'Night';
     }
 
-    // Function to display the clock information
-    function displayClockInfo(dayMoment, formattedTime) {
-        // Get the clock information element
-        var clockInfoElement = document.getElementById('clock-info');
+    // Actualizar elementos en la página
+    timeOfDayElement.textContent = `Time of Day: ${timeOfDay}`;
+    currentTimeElement.textContent = `Current Time: ${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
 
-        // Update the content
-        clockInfoElement.innerHTML = `
-            <p>Time of day: ${dayMoment}</p>
-            <p>Current time: ${formattedTime}</p>
-        `;
-    }
+    // Actualizar cada segundo
+    setTimeout(updateClock, 1000);
+}
 
-    // Function to get the day moment
-    function getDayMoment(hours) {
-        if (hours >= 5 && hours < 7) {
-            return 'Dawn';
-        } else if (hours >= 7 && hours < 12) {
-            return 'Morning';
-        } else if (hours >= 12 && hours < 14) {
-            return 'Midday';
-        } else if (hours >= 14 && hours < 18) {
-            return 'Afternoon';
-        } else if (hours >= 18 && hours < 20) {
-            return 'Dusk';
-        } else if (hours >= 20 && hours < 24) {
-            return 'Night';
-        } else {
-            return 'Midnight';
-        }
-    }
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
 
-    // Function to format the time
-    function formatTime(hours, minutes, seconds) {
-        return (
-            padZero(hours) + ':' + padZero(minutes) + ':' + padZero(seconds)
-        );
-    }
-
-    // Function to pad zero to the left if necessary
-    function padZero(number) {
-        return number < 10 ? '0' + number : number;
-    }
-
-    // Start the clock
-    getTimeAndDayMoment();
-});
+// Iniciar la actualización del reloj al cargar la página
+updateClock();
