9985091f4fa443eff6d8468774558d45583babbb
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-22 16:55:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var clockContainer = document.getElementById('clock-container');
+    // Get the clock element
     var clockElement = document.getElementById('clock');
 
+    // Get the clock container element
+    var clockContainerElement = document.getElementById('clock-container');
+
+    // Function to get the current time and day moment
     function getTimeAndDayMoment() {
         var currentDate = new Date();
         var hours = currentDate.getHours();
         var minutes = currentDate.getMinutes();
         var seconds = currentDate.getSeconds();
 
+        // Determine the day moment
         var dayMoment = getDayMoment(hours);
+
+        // Format the current time
         var formattedTime = formatTime(hours, minutes, seconds);
 
-        clockElement.textContent = 'Time of day: ' + dayMoment + ' | Current time: ' + formattedTime;
+        // Display the result in the clock element
+        clockElement.textContent = formattedTime + ' ' + dayMoment;
 
+        // Display the result in the clock container
+        displayClockInfo(dayMoment, formattedTime);
+
+        // Update every second
         setTimeout(getTimeAndDayMoment, 1000);
     }
 
+    // Function to display the clock information
+    function displayClockInfo(dayMoment, formattedTime) {
+        // Get the clock information element
+        var clockInfoElement = document.getElementById('clock-info');
+
+        // Update the content
+        clockInfoElement.innerHTML = `
+            <p>Time of day: ${dayMoment}</p>
+            <p>Current time: ${formattedTime}</p>
+        `;
+    }
+
+    // Function to get the day moment
     function getDayMoment(hours) {
         if (hours >= 5 && hours < 7) {
             return 'Dawn';
@@ -34,19 +59,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    // Function to format the time
     function formatTime(hours, minutes, seconds) {
         return (
             padZero(hours) + ':' + padZero(minutes) + ':' + padZero(seconds)
         );
     }
 
+    // Function to pad zero to the left if necessary
     function padZero(number) {
         return number < 10 ? '0' + number : number;
     }
 
-    // Iniciar con "loading"
-    clockElement.textContent = 'Time of day: Loading | Current time: Loading';
-
-    // Obtener los datos
+    // Start the clock
     getTimeAndDayMoment();
 });
