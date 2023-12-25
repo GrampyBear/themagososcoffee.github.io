77b7c078f0b426ad97eb6e763436360a525e70f2
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-25 00:47:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function updateClock() {
         const timeOfDayElement = document.getElementById('time-of-day');
         const currentTimeElement = document.getElementById('current-time');
+        const body = document.body;
 
         const now = new Date();
         const hours = now.getHours();
@@ -14,25 +15,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Determinar el momento del día
         let timeOfDay;
+        let gradientColors;
+        
         if (hours >= 5 && hours < 7) {
             timeOfDay = 'Dawn';
+            gradientColors = ['#FFA07A', '#FFE4B5', '#B0E0E6'];
         } else if (hours >= 7 && hours < 12) {
             timeOfDay = 'Morning';
+            gradientColors = ['#FFD700', '#FFA500', '#FF8C00'];
         } else if (hours >= 12 && hours < 17) {
-            timeOfDay = 'Noon';
+            timeOfDay = 'Midday';
+            gradientColors = ['#00CED1', '#20B2AA', '#5F9EA0'];
         } else if (hours >= 17 && hours < 20) {
             timeOfDay = 'Afternoon';
+            gradientColors = ['#FF4500', '#FF6347', '#FF7F50'];
         } else if (hours >= 20 && hours < 22) {
             timeOfDay = 'Dusk';
+            gradientColors = ['#FF69B4', '#FFB6C1', '#DB7093'];
         } else if (hours >= 22 && hours <= 23) {
             timeOfDay = 'Night';
+            gradientColors = ['#000080', '#191970', '#00008B'];
         } else if (hours >= 0 && hours < 5) {
             timeOfDay = 'Midnight';
+            gradientColors = ['#2F4F4F', '#696969', '#808080'];
         }
 
         // Actualizar elementos en la página
         timeOfDayElement.textContent = `Time of Day: ${timeOfDay}`;
         currentTimeElement.textContent = `Current Time: ${formatTime(hours)}:${formatTime(minutes)}:${formatTime(seconds)}`;
+
+        applyGradientBackground(body, gradientColors);
 
         // Actualizar cada segundo
         setTimeout(updateClock, 1000);
@@ -40,6 +52,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function formatTime(time) {
         return time < 10 ? `0${time}` : time;
+    }
+
+    function applyGradientBackground(element, colors) {
+        const gradientStyle = `linear-gradient(to right, ${colors.join(', ')})`;
+        element.style.transition = 'background 10s ease-in-out';
+        element.style.background = gradientStyle;
     }
 
     // Iniciar la actualización del reloj al cargar la página
