c33fa1aa5e4133957db44b80faf32272c5751806
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-26 14:42:34 -0300

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -1,15 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const panicButton = document.getElementById('panic-button');
-    const mainContent = document.getElementById('main-content');
+    const contentToToggle = document.querySelectorAll('.content-to-toggle'); // Agrega la clase 'content-to-toggle' a los elementos que deseas ocultar/mostrar
+
+    let isPanicMode = false;
 
     panicButton.addEventListener('click', function () {
-        // Alternar la visibilidad del contenido principal
-        mainContent.classList.toggle('hidden');
+        isPanicMode = !isPanicMode;
 
-        // Cambiar el texto del botón según la visibilidad actual
-        const buttonText = mainContent.classList.contains('hidden') ? 'Show Content' : 'Hide Content';
-        panicButton.textContent = buttonText;
+        contentToToggle.forEach(element => {
+            element.style.display = isPanicMode ? 'none' : 'block';
+        });
+
+        // También puedes cambiar el color de fondo, etc., según tus preferencias
+        document.body.style.backgroundColor = isPanicMode ? '#000' : '#fff';
     });
-
-    // Resto del código...
 });
