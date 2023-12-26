8ed46e4f4d1aec2d60e8d68107c0c5ff25d2b627
Magoso <70547408+GrampyBear@users.noreply.github.com>
2023-12-26 12:07:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,11 +35,9 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (hours >= 22 && hours <= 23) {
             timeOfDay = 'Night';
             gradientColors = ['#33334c', '#404066', '#4d4d80'];
-            createStars();
         } else if (hours >= 0 && hours < 5) {
             timeOfDay = 'Midnight';
             gradientColors = ['#0a0a0a', '#262626', '#404040'];
-            createStars();
         }
 
         // Actualizar elementos en la página
@@ -60,20 +58,6 @@ document.addEventListener("DOMContentLoaded", function () {
         const gradientStyle = `linear-gradient(to bottom, ${colors[0]}, ${colors[1]})`;
         element.style.transition = 'background 10s linear';
         element.style.background = gradientStyle;
-    }
-
-        function createStars() {
-        const starsContainer = document.createElement('div');
-        starsContainer.classList.add('stars');
-        document.body.appendChild(starsContainer);
-
-        for (let i = 0; i < 50; i++) {
-            const star = document.createElement('div');
-            star.classList.add('star');
-            star.style.left = `${Math.random() * 100}vw`;
-            star.style.top = `${Math.random() * 100}vh`;
-            starsContainer.appendChild(star);
-        }
     }
 
     // Iniciar la actualización del reloj al cargar la página
